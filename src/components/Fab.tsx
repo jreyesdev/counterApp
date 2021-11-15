f5aa3a496b240b2f1b4c072bf0738f49ed327be6
jreyesdev <djrm1204@gmail.com>
2021-11-15 16:57:54 -0400

    Componente Boton con tipo de plataforma

---
+++ src/components/Fab.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+export const Fab = ({position = 'br', title, onPress}: Props) => {
+  const TouchIOS = () => {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.75}
+        onPress={onPress}
+        style={[
+          styles.fabLocation,
+          position === 'br' ? styles.right : styles.left,
+        ]}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const TouchAndroid = () => {
+    return (
+      <View
+        style={[
+          styles.fabLocation,
+          position === 'br' ? styles.right : styles.left,
+        ]}>
+        <TouchableNativeFeedback
+          onPress={onPress}
+          background={TouchableNativeFeedback.Ripple('black', true, 30)}>
+          <View style={styles.fab}>
+            <Text style={styles.fabText}>{title}</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
+    );
+  };
+
+  return Platform.OS === 'ios' ? <TouchIOS /> : <TouchAndroid />;
+};
+
+const styles = StyleSheet.create({
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+  right: {
+    right: 25,
+  },
+  left: {
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 4,
+      width: 0,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,25 +1,14 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Fab} from '../components/Fab';
 
 export const ContadorScreen = () => {
-  const [cont, setCont] = useState(10);
+  const [cont, setCont] = useState(0);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Contador: {cont}</Text>
-      <TouchableOpacity
-        style={styles.fabLocationBL}
-        onPress={() => setCont(cont - 1)}>
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>-1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.fabLocationBR}
-        onPress={() => setCont(cont + 1)}>
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>+1</Text>
-        </View>
-      </TouchableOpacity>
+      <Fab title="-1" position="bl" onPress={() => setCont(cont - 1)} />
+      <Fab title="+1" onPress={() => setCont(cont + 1)} />
     </View>
   );
 };
@@ -33,28 +22,5 @@ const styles = StyleSheet.create({
     fontSize: 50,
     textAlign: 'center',
     top: -15,
-  },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 25,
-    left: 25,
-  },
-  fabLocationBR: {
-    position: 'absolute',
-    bottom: 25,
-    right: 25,
-  },
-  fab: {
-    backgroundColor: '#5856D6',
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    justifyContent: 'center',
-  },
-  fabText: {
-    color: 'white',
-    fontSize: 25,
-    fontWeight: 'bold',
-    alignSelf: 'center',
   },
 });
